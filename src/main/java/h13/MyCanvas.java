a10866e0ec8cfb499373f53a45fb66f32754039a
Rdeisenroth <rdeisenroth@gmail.com>
2022-02-03 23:57:34 +0100

    Cleanup MyCanvas

--- src/main/java/h13/MyCanvas.java
+++ src/main/java/h13/MyCanvas.java
@@ -1,13 +1,17 @@
 package h13;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
 
 /**
  *
@@ -100,31 +104,116 @@ public class MyCanvas extends java.awt.Canvas {
 
     }
 
+    /**
+     * Fills a Given Shape and also draws a border with the given Colors.
+     *
+     * @param g2d           the specified Graphics context
+     * @param interiorColor the Color of the filled Area
+     * @param borderColor   the border Color
+     * @param borderWidth   the Width of the Border
+     * @param s             the Shape to draw
+     */
+    private void fillDraw(Graphics2D g2d, Color interiorColor, Color borderColor, int borderWidth, Shape s) {
+        g2d.setColor(interiorColor);
+        g2d.fill(s);
+        var oldStroke = g2d.getStroke();
+        g2d.setStroke(new BasicStroke(borderWidth));
+        g2d.setColor(borderColor);
+        g2d.draw(s);
+        g2d.setStroke(oldStroke);
+    }
+
+    /**
+     * Fills a Given Shape and also draws a border with the given Colors and Scale
+     * to the
+     * Center of the Screen.
+     *
+     * @param g2d           the specified Graphics context
+     * @param interiorColor the Color of the filled Area
+     * @param borderColor   the border Color
+     * @param borderWidth   the Width of the Border
+     * @param s             the Shape to draw
+     * @param scaleX        how much horizontal screen space the shape will take
+     *                      (1.0d -> full screen)
+     * @param scaleY        how much vertical screen space the shape will take
+     *                      (1.0d -> full screen)
+     */
+    private void fillDrawCentered(Graphics2D g2d, Color interiorColor, Color borderColor, int borderWidth,
+            RectangularShape s, double scaleX, double ScaleY) {
+        centerShape(s, scaleX, ScaleY, borderWidth);
+        fillDraw(g2d, interiorColor, borderColor, borderWidth, s);
+    }
+
+    /**
+     * Centers a rectangular Shape
+     *
+     * @param <T>         The Dynamic Type of the RectangularShape
+     * @param s           the RectangularShape
+     * @param scaleX      how much horizontal screen space the shape will take
+     *                    (1.0d -> full screen)
+     * @param scaleY      how much vertical screen space the shape will take
+     *                    (1.0d -> full screen)
+     * @param borderWidth the Border width to consider
+     * @return the centered Shape
+     */
+    private <T extends RectangularShape> void centerShape(T s, double scaleX, double scaleY, int borderWidth) {
+        Rectangle bounds = getBounds();
+        s.setFrameFromCenter(
+                bounds.getCenterX(),
+                bounds.getCenterY(),
+                bounds.getCenterX() - bounds.getCenterX() * scaleX + borderWidth / 2,
+                bounds.getCenterY() - bounds.getCenterY() * scaleY + borderWidth / 2);
+    }
+
+    /**
+     * Returns a Color generated from the original Color with the desired
+     * transparency (alpha).
+     *
+     * @param c     The Source color
+     * @param alpha the desired Alpha
+     * @return the generated Color
+     */
+    private Color colorWithAlpha(Color c, int alpha) {
+        return new Color(c.getRed(), c.getGreen(), c.getBlue(), alpha);
+    }
+
+    /**
+     * Returns a Color generated from the original Color with the desired
+     * transparency (alpha).
+     *
+     * @param c     The Source color
+     * @param alpha the desired Alpha
+     * @return the generated Color
+     */
+    private Color colorWithAlpha(Color c, float alpha) {
+        return colorWithAlpha(c, (int) (alpha * 255 + 0.5));
+    }
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
         // Paint all Three Figures to the Center of the Screen
         Rectangle bounds = getBounds();
-        Point2D.Double center = new Point2D.Double(bounds.getCenterX(), bounds.getCenterY());
-        System.out.println(bounds.toString());
         // Green Ellipse
-        g.setColor(Color.GREEN);
-        g2d.draw(
-                new Ellipse2D.Double(
-                        bounds.getCenterX() - 0.5 * 0.9 * bounds.getWidth() * zoom,
-                        bounds.getCenterY() - 0.5 * 0.9 * bounds.getHeight() * zoom,
-                        0.9 * bounds.getWidth() * zoom,
-                        0.9 * bounds.getHeight() * zoom));
+        fillDrawCentered(g2d,
+                colorWithAlpha(Color.GREEN, 0.5f),
+                Color.GREEN,
+                20,
+                new Ellipse2D.Double(),
+                0.9 * zoom,
+                0.9 * zoom);
 
         // Yellow Rectangle
-        g.setColor(Color.YELLOW);
-        g2d.draw(
-                new Rectangle2D.Double(
-                        bounds.getCenterX() - 0.5 * 0.8 * bounds.getWidth() * zoom,
-                        bounds.getCenterY() - 0.5 * 0.8 * bounds.getHeight() * zoom,
-                        0.8 * bounds.getWidth() * zoom,
-                        0.8 * bounds.getHeight() * zoom));
+        fillDrawCentered(g2d,
+                colorWithAlpha(Color.YELLOW, 0.5f),
+                Color.YELLOW,
+                20,
+                new Rectangle2D.Double(),
+                0.8 * zoom,
+                0.8 * zoom);
 
         // Blue String
         g.setColor(Color.BLUE);
@@ -132,8 +221,12 @@ public class MyCanvas extends java.awt.Canvas {
         int fontWidth = g.getFontMetrics(font).stringWidth(text);
         var fontSize = (float) ((bounds.width * zoom) / fontWidth) * font.getSize();
         var newFont = g.getFont().deriveFont(fontSize);
+        var newFontMetrics = g.getFontMetrics(font);
         this.font = newFont;
-        // g2d.sca
-        g2d.drawString(text, 0, 0);
+        // g2d.scale
+        g2d.drawString(
+                text,
+                (int) bounds.getCenterX() - newFontMetrics.stringWidth(text) / 2,
+                (int) bounds.getCenterY() + newFontMetrics.getHeight() / 2);
     }
 }
