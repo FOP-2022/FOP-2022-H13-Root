e4b528aaf777fc8de9c670516f060f332b0325e0
Ruben Deisenroth <rdeisenroth@gmail.com>
2022-02-13 02:09:36 +0100

    H1_1 Tests Done

--- src/grader/java/h13/Tutor_Test_H1_1.java
+++ src/grader/java/h13/Tutor_Test_H1_1.java
@@ -1,21 +1,13 @@
 package h13;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Toolkit;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.util.Random;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
-
-import h13.MyPanel;
 
 public class Tutor_Test_H1_1 {
     @Test
@@ -32,6 +24,51 @@ public class Tutor_Test_H1_1 {
 
     @Test
     void testAddShapeMethods() {
+        var mp = new MyPanel();
+        var mpt = new MyPanelTutor();
+        List<MyPanel.Figure> mpStandartFigs = new ArrayList<>(List.of(
+                MyPanel.Figure.BLUE_STRING,
+                MyPanel.Figure.YELLOW_RECTANGLE,
+                MyPanel.Figure.GREEN_ELLIPSE));
+        List<MyPanelTutor.Figure> mptStandartFigs = new ArrayList<>(List.of(
+                MyPanelTutor.Figure.BLUE_STRING,
+                MyPanelTutor.Figure.YELLOW_RECTANGLE,
+                MyPanelTutor.Figure.GREEN_ELLIPSE));
 
+        Stream.of(
+                new Runnable[] {
+                        () -> {
+                            mp.addBlueString();
+                            mpt.addBlueString();
+                        },
+                        () -> {
+                            mp.addYellowRectangle();
+                            mpt.addYellowRectangle();
+                        },
+                        () -> {
+                            mp.addGreenEllipse();
+                            mpt.addGreenEllipse();
+                        }
+                })
+                .forEach(addMethods -> {
+                    mp.figuresToDisplay = new ArrayList<>();
+                    mpt.figuresToDisplay = new ArrayList<>();
+
+                    // Add One Figure
+                    addMethods.run();
+                    assertEquals(mp.figuresToDisplay.get(0).name(), mpt.figuresToDisplay.get(0).name());
+
+                    // Add Figure Again, whilst having to move it
+                    mp.figuresToDisplay = new ArrayList<>(mpStandartFigs);
+                    mpt.figuresToDisplay = new ArrayList<>(mptStandartFigs);
+                    addMethods.run();
+                    assertEquals(mp.figuresToDisplay.size(), mpt.figuresToDisplay.size());
+                    assertEquals(mp.figuresToDisplay.get(0).name(), mpt.figuresToDisplay.get(0).name());
+                    assertEquals(mp.figuresToDisplay.get(1).name(), mpt.figuresToDisplay.get(1).name());
+                    assertEquals(mp.figuresToDisplay.get(2).name(), mpt.figuresToDisplay.get(2).name());
+
+                    Collections.rotate(mpStandartFigs, 1);
+                    Collections.rotate(mptStandartFigs, 1);
+                });
     }
 }
