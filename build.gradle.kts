2854e6fc9e2464eef48c6ace8dc97f28b361a337
Alexander Staeding <alexanderstaeding@gmail.com>
2022-02-28 14:55:05 +0100

    Fix buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import org.sourcegrade.submitter.submit
+
 plugins {
     java
     application
     id("org.sourcegrade.style") version "1.2.0"
+    id("org.sourcegrade.submitter") version "0.4.0"
 }
 
 version = "0.1.0-SNAPSHOT"
@@ -11,18 +14,30 @@ repositories {
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
 }
 
+submit {
+    assignmentId = "h13"
+    studentId = "ab12cdef"
+    firstName = "sol_first"
+    lastName = "sol_last"
+}
+
 val grader: SourceSet by sourceSets.creating {
-    compileClasspath += sourceSets.test.get().compileClasspath
-    runtimeClasspath += output + compileClasspath
+    val test = sourceSets.test.get()
+    compileClasspath += test.output + test.compileClasspath
+    runtimeClasspath += output + test.runtimeClasspath
 }
 
 dependencies {
-    "graderImplementation"("org.sourcegrade:jagr-launcher:0.4.0-SNAPSHOT")
+    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("com.formdev:flatlaf:2.0.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    "graderCompileOnly"("org.sourcegrade:jagr-launcher:0.4.0-SNAPSHOT")
+    "graderImplementation"("org.ow2.asm:asm-util:9.2")
+    "graderImplementation"("com.google.guava:guava:31.0.1-jre")
 }
 
 application {
-    mainClass.set("h13.Main")
+    mainClass.set("h13.TwoSimpleWindows")
 }
 
 tasks {
@@ -38,7 +53,7 @@ tasks {
     named("check") {
         dependsOn(graderTest)
     }
-    create<Jar>("graderJar") {
+    val graderJar by creating(Jar::class) {
         group = "build"
         afterEvaluate {
             archiveFileName.set("FOP-2022-H13-${project.version}.jar")
@@ -47,8 +62,38 @@ tasks {
             from(grader.allSource)
         }
     }
+    val graderLibs by creating(Jar::class) {
+        group = "build"
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+        // don't include Jagr's runtime dependencies
+        val jagrRuntime = configurations["graderCompileClasspath"]
+            .resolvedConfiguration
+            .firstLevelModuleDependencies
+            .first { it.moduleGroup == "org.sourcegrade" && it.moduleName == "jagr-launcher" }
+            .allModuleArtifacts
+            .map { it.file }
+
+        val runtimeDeps = grader.runtimeClasspath.mapNotNull {
+            if (it.path.toLowerCase().contains("h13") || jagrRuntime.contains(it)) {
+                null
+            } else if (it.isDirectory) {
+                it
+            } else {
+                zipTree(it)
+            }
+        }
+        from(runtimeDeps)
+        archiveFileName.set("FOP-2022-h13-${project.version}-libs.jar")
+    }
+    create("graderAll") {
+        group = "build"
+        dependsOn(graderJar, graderLibs)
+    }
     withType<JavaCompile> {
         options.encoding = "UTF-8"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
     jar {
         enabled = false
